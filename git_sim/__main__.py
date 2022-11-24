2ac2ecaa6e1ec788c3fc119de08661879a8341ff
Jacob Stopak <jacob@initialcommit.io>
2022-11-24 12:38:09 -0800

    Clean up command ordering for consistency
    Signed-off-by: Jacob Stopak <jacob@initialcommit.io>

--- git_sim/__main__.py
+++ git_sim/__main__.py
@@ -9,67 +9,62 @@ from manim.utils.file_ops import open_file as open_media_file
 
 def main():
     parser = argparse.ArgumentParser("git-sim", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--hide-merged-chains", help="Hide commits from merged branches, i.e. only display mainline commits", action="store_true")
-    parser.add_argument("--reverse", help="Display commits in reverse order in the Git animation", action="store_true")
     parser.add_argument("--title", help="Custom title to display at the beginning of the animation", type=str, default="Git Sim, by initialcommit.com")
     parser.add_argument("--logo", help="The path to a custom logo to use in the animation intro/outro", type=str, default=os.path.join(str(pathlib.Path(__file__).parent.resolve()), "logo.png"))
     parser.add_argument("--outro-top-text", help="Custom text to display above the logo during the outro", type=str, default="Thanks for using Initial Commit!")
     parser.add_argument("--outro-bottom-text", help="Custom text to display below the logo during the outro", type=str, default="Learn more at initialcommit.com")
     parser.add_argument("--show-intro", help="Add an intro sequence with custom logo and title", action="store_true")
     parser.add_argument("--show-outro", help="Add an outro sequence with custom logo and text", action="store_true")
-    parser.add_argument("--max-branches-per-commit", help="Maximum number of branch labels to display for each commit", type=int, default=2)
-    parser.add_argument("--max-tags-per-commit", help="Maximum number of tags to display for each commit", type=int, default=1)
     parser.add_argument("--media-dir", help="The path to output the animation data and video file", type=str, default=".")
     parser.add_argument("--low-quality", help="Render output video in low quality, useful for faster testing", action="store_true")
     parser.add_argument("--light-mode", help="Enable light-mode with white background", action="store_true")
-    parser.add_argument("--invert-branches", help="Invert positioning of branches where applicable", action="store_true")
     parser.add_argument("--speed", help="A multiple of the standard 1x animation speed (ex: 2 = twice as fast, 0.5 = half as fast)", type=float, default=1.5)
     parser.add_argument("--animate", help="Animate the simulation and output as an mp4 video", action="store_true")
 
     subparsers = parser.add_subparsers(dest="subcommand", help="subcommand help")
 
-    reset = subparsers.add_parser("reset", help="reset help")
+    log = subparsers.add_parser("log", help="log -h")
+    log.add_argument("--commits", help="The number of commits to display in the simulated log output", type=int, default=8)
+
+    status = subparsers.add_parser("status", help="status -h")
+
+    add = subparsers.add_parser("add", help="add -h")
+    add.add_argument("name", nargs="+", help="The names of one or more files to add to Git's staging area", type=str) 
+
+    restore = subparsers.add_parser("restore", help="restore -h")
+    restore.add_argument("name", nargs="+", help="The names of one or more files to restore", type=str) 
+
+    commit = subparsers.add_parser("commit", help="commit -h")
+    commit.add_argument("-m", "--message", help="The commit message of the new commit", type=str, default="New commit")
+
+    stash = subparsers.add_parser("stash", help="stash -h")
+    stash.add_argument("name", nargs="*", help="The name of the file to stash changes for", type=str)
+
+    branch = subparsers.add_parser("branch", help="branch -h")
+    branch.add_argument("name", help="The name of the new branch", type=str)
+
+    tag = subparsers.add_parser("tag", help="tag -h")
+    tag.add_argument("name", help="The name of the new tag", type=str)
+
+    reset = subparsers.add_parser("reset", help="reset -h")
     reset.add_argument("commit", nargs="?", help="The ref (branch/tag), or commit ID to simulate reset to", type=str, default="HEAD")
     reset.add_argument("--mode", help="Either mixed (default), soft, or hard", type=str, default="default")
     reset.add_argument("--soft", help="Simulate a soft reset, shortcut for --mode=soft", action="store_true")
     reset.add_argument("--mixed", help="Simulate a mixed reset, shortcut for --mode=mixed", action="store_true")
     reset.add_argument("--hard", help="Simulate a soft reset, shortcut for --mode=hard", action="store_true")
 
-    revert = subparsers.add_parser("revert", help="revert help")
+    revert = subparsers.add_parser("revert", help="revert -h")
     revert.add_argument("commit", nargs="?", help="The ref (branch/tag), or commit ID to simulate revert", type=str, default="HEAD")
 
-    branch = subparsers.add_parser("branch", help="branch help")
-    branch.add_argument("name", help="The name of the new branch", type=str)
-
-    tag = subparsers.add_parser("tag", help="tag help")
-    tag.add_argument("name", help="The name of the new tag", type=str)
-
-    status = subparsers.add_parser("status", help="status help")
-
-    add = subparsers.add_parser("add", help="add help")
-    add.add_argument("name", nargs="+", help="The names of one or more files to add to Git's staging area", type=str) 
-
-    commit = subparsers.add_parser("commit", help="commit help")
-    commit.add_argument("-m", "--message", help="The commit message of the new commit", type=str, default="New commit")
-
-    stash = subparsers.add_parser("stash", help="stash help")
-    stash.add_argument("name", nargs="*", help="The name of the file to stash changes for", type=str)
-
-    restore = subparsers.add_parser("restore", help="restore help")
-    restore.add_argument("name", nargs="+", help="The names of one or more files to restore", type=str) 
-
-    log = subparsers.add_parser("log", help="log help")
-    log.add_argument("--commits", help="The number of commits to display in the simulated log output", type=int, default=8)
-
-    cherrypick = subparsers.add_parser("cherry-pick", help="cherry-pick help")
-    cherrypick.add_argument("commit", nargs=1, type=str, help="The ref (branch/tag), or commit ID to simulate cherry-pick onto active branch")
-
-    merge = subparsers.add_parser("merge", help="merge help")
+    merge = subparsers.add_parser("merge", help="merge -h")
     merge.add_argument("branch", nargs=1, type=str, help="The name of the branch to merge into the active checked-out branch")
     merge.add_argument("--no-ff", help="Simulate creation of a merge commit in all cases, even when the merge could instead be resolved as a fast-forward", action="store_true")
 
-    rebase = subparsers.add_parser("rebase", help="rebase help")
+    rebase = subparsers.add_parser("rebase", help="rebase -h")
     rebase.add_argument("branch", nargs=1, type=str, help="The branch to simulate rebasing the checked-out commit onto")
+
+    cherrypick = subparsers.add_parser("cherry-pick", help="cherry-pick -h")
+    cherrypick.add_argument("commit", nargs=1, type=str, help="The ref (branch/tag), or commit ID to simulate cherry-pick onto active branch")
 
     args = parser.parse_args()
 
--- git_sim/git_sim.py
+++ git_sim/git_sim.py
@@ -1,19 +1,19 @@
 from manim import *
 import git, sys, numpy
 
-from git_sim.git_sim_reset import *
-from git_sim.git_sim_revert import *
-from git_sim.git_sim_branch import *
-from git_sim.git_sim_tag import *
+from git_sim.git_sim_log import *
 from git_sim.git_sim_status import *
 from git_sim.git_sim_add import *
+from git_sim.git_sim_restore import *
 from git_sim.git_sim_commit import *
 from git_sim.git_sim_stash import *
-from git_sim.git_sim_restore import *
-from git_sim.git_sim_log import *
-from git_sim.git_sim_cherrypick import *
+from git_sim.git_sim_branch import *
+from git_sim.git_sim_tag import *
+from git_sim.git_sim_reset import *
+from git_sim.git_sim_revert import *
 from git_sim.git_sim_merge import *
 from git_sim.git_sim_rebase import *
+from git_sim.git_sim_cherrypick import *
 
 class GitSim(MovingCameraScene):
     def __init__(self, args):
@@ -26,31 +26,31 @@ class GitSim(MovingCameraScene):
             self.fontColor = WHITE
 
     def construct(self):
-        if self.args.subcommand == 'reset':
-            self.command = GitSimReset(self)
-        elif self.args.subcommand == 'revert':
-            self.command = GitSimRevert(self)
-        elif self.args.subcommand == 'branch':
-            self.command = GitSimBranch(self)
-        elif self.args.subcommand == 'tag':
-            self.command = GitSimTag(self)
+        if self.args.subcommand == 'log':
+            self.command = GitSimLog(self)
         elif self.args.subcommand == 'status':
             self.command = GitSimStatus(self)
         elif self.args.subcommand == 'add':
             self.command = GitSimAdd(self)
+        elif self.args.subcommand == 'restore':
+            self.command = GitSimRestore(self)
         elif self.args.subcommand == 'commit':
             self.command = GitSimCommit(self)
         elif self.args.subcommand == 'stash':
             self.command = GitSimStash(self)
-        elif self.args.subcommand == 'restore':
-            self.command = GitSimRestore(self)
-        elif self.args.subcommand == 'log':
-            self.command = GitSimLog(self)
-        elif self.args.subcommand == 'cherry-pick':
-            self.command = GitSimCherryPick(self)
+        elif self.args.subcommand == 'branch':
+            self.command = GitSimBranch(self)
+        elif self.args.subcommand == 'tag':
+            self.command = GitSimTag(self)
+        elif self.args.subcommand == 'reset':
+            self.command = GitSimReset(self)
+        elif self.args.subcommand == 'revert':
+            self.command = GitSimRevert(self)
         elif self.args.subcommand == 'merge':
             self.command = GitSimMerge(self)
         elif self.args.subcommand == 'rebase':
             self.command = GitSimRebase(self)
+        elif self.args.subcommand == 'cherry-pick':
+            self.command = GitSimCherryPick(self)
 
         self.command.execute()
