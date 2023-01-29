a6d739aa03100b71f00fd2d5cc8c4e483103428f
Manuel Stausberg <m.stausberg@pm.me>
2023-01-29 18:57:31 +0100

    convert GitSimReset to new inheritance model

--- git_sim/git_sim_reset.py
+++ git_sim/git_sim_reset.py
@@ -1,28 +1,28 @@
 import sys
+from argparse import Namespace
 
 import git
 import manim as m
-import numpy
 
 from git_sim.git_sim_base_command import GitSimBaseCommand
 
 
 class GitSimReset(GitSimBaseCommand):
-    def __init__(self, scene):
-        super().__init__(scene)
+    def __init__(self, args: Namespace):
+        super().__init__(args=args)
 
         try:
-            self.resetTo = git.repo.fun.rev_parse(self.repo, self.scene.args.commit)
+            self.resetTo = git.repo.fun.rev_parse(self.repo, self.args.commit)
         except git.exc.BadName:
             print(
                 "git-sim error: '"
-                + self.scene.args.commit
+                + self.args.commit
                 + "' is not a valid Git ref or identifier."
             )
             sys.exit(1)
 
         self.commitsSinceResetTo = list(
-            self.repo.iter_commits(self.scene.args.commit + "...HEAD")
+            self.repo.iter_commits(self.args.commit + "...HEAD")
         )
         self.maxrefs = 2
         self.hide_first_tag = True
@@ -32,24 +32,20 @@ class GitSimReset(GitSimBaseCommand):
         except TypeError:
             pass
 
-        if self.scene.args.hard:
-            self.scene.args.mode = "hard"
-        if self.scene.args.mixed:
-            self.scene.args.mode = "mixed"
-        if self.scene.args.soft:
-            self.scene.args.mode = "soft"
+        if self.args.hard:
+            self.args.mode = "hard"
+        if self.args.mixed:
+            self.args.mode = "mixed"
+        if self.args.soft:
+            self.args.mode = "soft"
 
-    def execute(self):
+    def construct(self):
         print(
             "Simulating: git "
-            + self.scene.args.subcommand
-            + (
-                " --" + self.scene.args.mode
-                if self.scene.args.mode != "default"
-                else ""
-            )
+            + self.args.subcommand
+            + (" --" + self.args.mode if self.args.mode != "default" else "")
             + " "
-            + self.scene.args.commit
+            + self.args.commit
         )
 
         self.show_intro()
@@ -66,15 +62,13 @@ class GitSimReset(GitSimBaseCommand):
     def build_commit_id_and_message(self, commit, dots=False):
         hide_refs = False
         if commit == "dark":
-            commitId = m.Text(
-                "", font="Monospace", font_size=20, color=self.scene.fontColor
-            )
+            commitId = m.Text("", font="Monospace", font_size=20, color=self.fontColor)
             commitMessage = ""
         elif self.i == 3 and self.resetTo.hexsha not in [
             commit.hexsha for commit in self.get_nondark_commits()
         ]:
             commitId = m.Text(
-                "...", font="Monospace", font_size=20, color=self.scene.fontColor
+                "...", font="Monospace", font_size=20, color=self.fontColor
             )
             commitMessage = "..."
             hide_refs = True
@@ -85,7 +79,7 @@ class GitSimReset(GitSimBaseCommand):
                 self.resetTo.hexsha[:6],
                 font="Monospace",
                 font_size=20,
-                color=self.scene.fontColor,
+                color=self.fontColor,
             )
             commitMessage = self.resetTo.message.split("\n")[0][:40].replace("\n", " ")
             commit = self.resetTo
@@ -95,7 +89,7 @@ class GitSimReset(GitSimBaseCommand):
                 commit.hexsha[:6],
                 font="Monospace",
                 font_size=20,
-                color=self.scene.fontColor,
+                color=self.fontColor,
             )
             commitMessage = commit.message.split("\n")[0][:40].replace("\n", " ")
 
@@ -120,33 +114,27 @@ class GitSimReset(GitSimBaseCommand):
             if commit.hexsha == self.resetTo.hexsha:
                 break
             for filename in commit.stats.files:
-                if self.scene.args.mode == "soft":
+                if self.args.mode == "soft":
                     thirdColumnFileNames.add(filename)
-                elif (
-                    self.scene.args.mode == "mixed" or self.scene.args.mode == "default"
-                ):
+                elif self.args.mode == "mixed" or self.args.mode == "default":
                     secondColumnFileNames.add(filename)
-                elif self.scene.args.mode == "hard":
+                elif self.args.mode == "hard":
                     firstColumnFileNames.add(filename)
 
         for x in self.repo.index.diff(None):
             if "git-sim_media" not in x.a_path:
-                if self.scene.args.mode == "soft":
+                if self.args.mode == "soft":
                     secondColumnFileNames.add(x.a_path)
-                elif (
-                    self.scene.args.mode == "mixed" or self.scene.args.mode == "default"
-                ):
+                elif self.args.mode == "mixed" or self.args.mode == "default":
                     secondColumnFileNames.add(x.a_path)
-                elif self.scene.args.mode == "hard":
+                elif self.args.mode == "hard":
                     firstColumnFileNames.add(x.a_path)
 
         for y in self.repo.index.diff("HEAD"):
             if "git-sim_media" not in y.a_path:
-                if self.scene.args.mode == "soft":
+                if self.args.mode == "soft":
                     thirdColumnFileNames.add(y.a_path)
-                elif (
-                    self.scene.args.mode == "mixed" or self.scene.args.mode == "default"
-                ):
+                elif self.args.mode == "mixed" or self.args.mode == "default":
                     secondColumnFileNames.add(y.a_path)
-                elif self.scene.args.mode == "hard":
+                elif self.args.mode == "hard":
                     firstColumnFileNames.add(y.a_path)
