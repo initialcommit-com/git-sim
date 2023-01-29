b70dfc58acb4997eee3b02214fd8a4cc2d1a4aeb
Manuel Stausberg <m.stausberg@pm.me>
2023-01-29 18:15:38 +0100

    convert GitSimCherryPick to new inheritance model

--- git_sim/git_sim_cherrypick.py
+++ git_sim/git_sim_cherrypick.py
@@ -1,48 +1,43 @@
 import sys
+from argparse import Namespace
 
 import git
 import manim as m
-import numpy
 
 from git_sim.git_sim_base_command import GitSimBaseCommand
 
 
 class GitSimCherryPick(GitSimBaseCommand):
-    def __init__(self, scene):
-        super().__init__(scene)
+    def __init__(self, args: Namespace):
+        super().__init__(args=args)
 
         try:
-            git.repo.fun.rev_parse(self.repo, self.scene.args.commit[0])
+            git.repo.fun.rev_parse(self.repo, self.args.commit[0])
         except git.exc.BadName:
             print(
                 "git-sim error: '"
-                + self.scene.args.commit[0]
+                + self.args.commit[0]
                 + "' is not a valid Git ref or identifier."
             )
             sys.exit(1)
 
-        if self.scene.args.commit[0] in [branch.name for branch in self.repo.heads]:
-            self.selected_branches.append(self.scene.args.commit[0])
+        if self.args.commit[0] in [branch.name for branch in self.repo.heads]:
+            self.selected_branches.append(self.args.commit[0])
 
         try:
             self.selected_branches.append(self.repo.active_branch.name)
         except TypeError:
             pass
 
-    def execute(self):
-        print(
-            "Simulating: git "
-            + self.scene.args.subcommand
-            + " "
-            + self.scene.args.commit[0]
-        )
+    def construct(self):
+        print("Simulating: git " + self.args.subcommand + " " + self.args.commit[0])
 
         if self.repo.active_branch.name in self.repo.git.branch(
-            "--contains", self.scene.args.commit[0]
+            "--contains", self.args.commit[0]
         ):
             print(
                 "git-sim error: Commit '"
-                + self.scene.args.commit[0]
+                + self.args.commit[0]
                 + "' is already included in the history of active branch '"
                 + self.repo.active_branch.name
                 + "'."
@@ -53,7 +48,7 @@ class GitSimCherryPick(GitSimBaseCommand):
         self.get_commits()
         self.parse_commits(self.commits[0])
         self.orig_commits = self.commits
-        self.get_commits(start=self.scene.args.commit[0])
+        self.get_commits(start=self.args.commit[0])
         self.parse_commits(self.commits[0], shift=4 * m.DOWN)
         self.center_frame_on_commit(self.orig_commits[0])
         self.setup_and_draw_parent(self.orig_commits[0], self.commits[0].message)
