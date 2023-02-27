5dfedf1366e4b9aa2a34d5e97488a41017990cb1
Jacob Stopak <jacob@initialcommit.io>
2023-02-26 21:42:08 -0800

    Allow diverse branching structures in log subcommand
    Signed-off-by: Jacob Stopak <jacob@initialcommit.io>

--- git_sim/log.py
+++ git_sim/log.py
@@ -3,6 +3,8 @@ import typer
 from git_sim.animations import handle_animations
 from git_sim.git_sim_base_command import GitSimBaseCommand
 from git_sim.settings import settings
+import numpy
+import manim as m
 
 
 class Log(GitSimBaseCommand):
@@ -14,17 +16,157 @@ class Log(GitSimBaseCommand):
             self.selected_branches.append(self.repo.active_branch.name)
         except TypeError:
             pass
+        self.arrow_map = []
 
     def construct(self):
         if not settings.stdout:
             print(f"{settings.INFO_STRING} {type(self).__name__.lower()}")
         self.show_intro()
         self.get_commits()
-        self.parse_commits(self.commits[0])
+        self.parse_commits(self.commits[0], 0)
         self.recenter_frame()
         self.scale_frame()
         self.fadeout()
         self.show_outro()
+
+    def parse_commits(
+        self, commit, i, prevCircle=None, shift=numpy.array([0.0, 0.0, 0.0]), dots=False
+    ):
+        isNewCommit = commit.hexsha not in self.drawnCommits
+
+        if i < self.numCommits and commit in self.commits:
+            commitId, circle, arrow, hide_refs = self.draw_commit(
+                commit, prevCircle, shift, dots
+            )
+
+            if commit != "dark":
+                if not hide_refs and isNewCommit:
+                    self.draw_head(commit, commitId)
+                    self.draw_branch(commit)
+                    self.draw_tag(commit)
+                if [arrow.start.tolist(), arrow.end.tolist()] not in self.arrow_map:
+                    self.draw_arrow(prevCircle, arrow)
+                    self.arrow_map.append([arrow.start.tolist(), arrow.end.tolist()])
+                if i == 0 and len(self.drawnRefs) < 2:
+                    self.draw_dark_ref()
+
+            if i < len(self.commits) - 1:
+                i += 1
+                commitParents = list(commit.parents)
+                if len(commitParents) > 0:
+                    #    if ( self.args.invert_branches ):
+                    #        commitParents.reverse()
+
+                    #    if ( self.args.hide_merged_chains ):
+                    #        self.parseCommits(commitParents[0], i+1,  prevCircle, toFadeOut)
+                    # else:
+                    for p in range(len(commitParents)):
+                        self.parse_commits(commitParents[p], i, circle, dots=True)
+            else:
+                self.i = 0
+
+    def draw_commit(
+        self, commit, prevCircle, shift=numpy.array([0.0, 0.0, 0.0]), dots=False
+    ):
+        if commit == "dark":
+            commitFill = m.WHITE if settings.light_mode else m.BLACK
+        elif len(commit.parents) <= 1:
+            commitFill = m.RED
+        else:
+            commitFill = m.GRAY
+
+        circle = m.Circle(
+            stroke_color=commitFill, fill_color=commitFill, fill_opacity=0.25
+        )
+        circle.height = 1
+
+        if shift.any():
+            circle.shift(shift)
+
+        if prevCircle:
+            circle.next_to(
+                prevCircle, m.RIGHT if settings.reverse else m.LEFT, buff=1.5
+            )
+
+        while any((circle.get_center() == c).all() for c in self.get_centers()):
+            circle.next_to(circle, m.DOWN, buff=3.5)
+
+        isNewCommit = commit.hexsha not in self.drawnCommits
+
+        if isNewCommit:
+            start = (
+                prevCircle.get_center()
+                if prevCircle
+                else (m.LEFT if settings.reverse else m.RIGHT)
+            )
+            end = circle.get_center()
+        else:
+            circle.move_to(self.drawnCommits[commit.hexsha].get_center())
+            start = prevCircle.get_center()
+            end = self.drawnCommits[commit.hexsha].get_center()
+
+        arrow = m.Arrow(start, end, color=self.fontColor)
+
+        if commit == "dark":
+            arrow = m.Arrow(
+                start, end, color=m.WHITE if settings.light_mode else m.BLACK
+            )
+
+        length = numpy.linalg.norm(start - end) - (1.5 if start[1] == end[1] else 3)
+        arrow.set_length(length)
+        angle = arrow.get_angle()
+        lineRect = (
+            m.Rectangle(height=0.1, width=length, color="#123456")
+            .move_to(arrow.get_center())
+            .rotate(angle)
+        )
+
+        for commitCircle in self.drawnCommits.values():
+            inter = m.Intersection(lineRect, commitCircle)
+            if inter.has_points():
+                arrow = m.CurvedArrow(start, end)
+                if start[1] == end[1]:
+                    arrow.shift(UP * 1.25)
+                if start[0] < end[0] and start[1] == end[1]:
+                    arrow.flip(RIGHT).shift(UP)
+
+        commitId, commitMessage, commit, hide_refs = self.build_commit_id_and_message(
+            commit, dots
+        )
+        commitId.next_to(circle, m.UP)
+
+        if commit != "dark":
+            self.drawnCommitIds[commit.hexsha] = commitId
+
+        message = m.Text(
+            "\n".join(
+                commitMessage[j : j + 20] for j in range(0, len(commitMessage), 20)
+            )[:100],
+            font="Monospace",
+            font_size=14,
+            color=self.fontColor,
+        ).next_to(circle, m.DOWN)
+
+        if settings.animate and commit != "dark" and isNewCommit:
+            self.play(
+                self.camera.frame.animate.move_to(circle.get_center()),
+                m.Create(circle),
+                m.AddTextLetterByLetter(commitId),
+                m.AddTextLetterByLetter(message),
+                run_time=1 / settings.speed,
+            )
+        elif isNewCommit:
+            self.add(circle, commitId, message)
+        else:
+            return commitId, circle, arrow, hide_refs
+
+        if commit != "dark":
+            self.drawnCommits[commit.hexsha] = circle
+
+        self.toFadeOut.add(circle, commitId, message)
+        self.prevRef = commitId
+
+        return commitId, circle, arrow, hide_refs
 
 
 def log(
@@ -32,7 +174,6 @@ def log(
         default=settings.commits,
         help="The number of commits to display in the simulated log output",
         min=1,
-        max=12,
     ),
 ):
     scene = Log(commits=commits)
