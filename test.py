6bb1b8dd90411afc33b49409eefc13927f8b998b
Jacob Stopak <jacob@initialcommit.io>
2023-03-23 14:17:22 -0700

    Update test
    Signed-off-by: Jacob Stopak <jacob@initialcommit.io>

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
 import unittest, git, argparse
 from manim import *
 
-from git_sim.git_sim import GitSim
-
 
 class TestGitSim(unittest.TestCase):
     def test_git_sim(self):
         """Test git sim."""
-
-        gs = GitSim(argparse.Namespace())
 
         self.assertEqual(1, 1)
 
